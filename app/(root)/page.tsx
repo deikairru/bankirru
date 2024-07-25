3d2bf256f2990aec66aeaff5dda39a3d40021e09
deikairru <deikairru@gmail.com>
2024-07-25 18:18:48 +0700

    component: add headerbox component to main page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import HeaderBox from "@/components/HeaderBox";
 
 const Home = () => {
+  const LoggedIn = {
+    firstName: 'Deika'
+  }
+
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type='greeting'
+            title='Welcome'
+            user={LoggedIn?.firstName || 'Guest'}
+            subtext='Access and manage your account and transactions efficiently'
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
