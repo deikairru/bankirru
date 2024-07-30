e26249b94e4389b47749389683c960b30c721a7c
deikairru <deikairru@gmail.com>
2024-07-30 18:12:04 +0700

    component: add skeleton component on bank page, fix margin on mobile

---
+++ app/(root)/my-banks/loading.tsx
@@ -0,0 +1,45 @@
+import BankCard from '@/components/BankCard';
+import Copy from '@/components/Copy';
+import HeaderBox from '@/components/HeaderBox'
+import React from 'react'
+
+const MyBanksSkeleton = () => {
+  return (
+    <section className='flex flex-col justify-start gap-4 items-start'>
+      <div className="my-banks">
+        <HeaderBox
+          title="Loading Bank Accounts ..."
+          subtext="Please wait a moment."
+        />
+
+        <div className="space-y-4">
+          <h2 className="header-2">
+            Loading Your cards ...
+          </h2>
+          <div className="max-w-[740px]">
+            <div className="flex flex-col lg:flex-row lg:flex-wrap gap-6 ">
+              <div className="flex flex-col">
+                <div className="w-[320px] h-[190px] rounded-2xl bg-bank-gradient animate-pulse"></div>
+                <Copy title='Copy your shareable bank id' />
+              </div>
+              <div className="flex flex-col">
+                <div className="w-[320px] h-[190px] rounded-2xl bg-bank-gradient animate-pulse"></div>
+                <Copy title='Copy your shareable bank id' />
+              </div>
+              <div className="flex flex-col">
+                <div className="w-[320px] h-[190px] rounded-2xl bg-bank-gradient animate-pulse"></div>
+                <Copy title='Copy your shareable bank id' />
+              </div>
+              <div className="flex flex-col">
+                <div className="w-[320px] h-[190px] rounded-2xl bg-bank-gradient animate-pulse"></div>
+                <Copy title='Copy your shareable bank id' />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default MyBanksSkeleton
