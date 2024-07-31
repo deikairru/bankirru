6b6e5eaa41703482d64b5fd202e9ff4d1911182e
deikairru <deikairru@gmail.com>
2024-07-31 21:46:43 +0700

    bugfix: fix dynamic-force issues

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,8 +1,7 @@
-"use server";
 import { Client, Account, Databases, Users } from "node-appwrite";
 import { cookies } from "next/headers";
 
-export async function createSessionClient() {
+export function createSessionClient() {
   const client = new Client()
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
     .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
@@ -21,7 +20,7 @@ export async function createSessionClient() {
   };
 }
 
-export async function createAdminClient() {
+export function createAdminClient() {
   const client = new Client()
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
     .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
